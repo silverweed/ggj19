c53a547e9f59e20803c1ce7f1b01e4d22310633d
silverweed <silverweed1991@gmail.com>
2019-01-27 00:50:40 +0100

    better modulation of elements during night

--- level/Sky.gd
+++ level/Sky.gd
@@ -8,6 +8,7 @@ const DAYNIGHT_CYCLE = 60
 var time = 0
 var time_direction = 1
 var progressive_time = 0
+var cur_modulate = Color.white
 
 onready var sunmoon_start_rot = $SunMoonPivot.rotation
 
@@ -20,8 +21,10 @@ func _process(delta):
 	if progressive_time >= 2 * DAYNIGHT_CYCLE:
 		progressive_time = 0
 		
-	$Background.modulate = DAY_COLOR.linear_interpolate(NIGHT_COLOR, time / DAYNIGHT_CYCLE)
-	$Clouds.modulate = $Background.modulate
+	cur_modulate = DAY_COLOR.linear_interpolate(NIGHT_COLOR, time / DAYNIGHT_CYCLE)
+	$Background.modulate = cur_modulate
+	$Clouds.modulate = cur_modulate
+	$Forebackground.modulate = cur_modulate
 	
 	$SunMoonPivot.rotation = lerp(sunmoon_start_rot,
 			sunmoon_start_rot + 2 * PI, 0.5 * progressive_time / DAYNIGHT_CYCLE)
--- level/Walls.gd
+++ level/Walls.gd
@@ -1,4 +1,13 @@
 extends StaticBody2D
 
+export (NodePath) var sky_path
+
+onready var sky = get_node(sky_path)
+
+
 func _ready():
 	collision_layer |= preload("res://block/Block.gd").get_exclusive_collision_layer()
+	
+
+func _process(delta):
+	modulate = sky.cur_modulate.lightened(0.5)
