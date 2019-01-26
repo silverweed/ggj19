5d9a8ac56ca5897a32ef817d629f837f7ca3be73
silverweed <silverweed1991@gmail.com>
2019-01-26 13:18:41 +0100

    separate ground

--- player/Player.gd
+++ player/Player.gd
@@ -31,17 +31,15 @@ const MIN_ACCELERATION = 0.1
 var deceleration = 40
 var speed = 600
 var gravity = Vector2(0, NORMAL_GRAVITY)
-
-var grabbable_block : Block = null
 var prev_velocity = Vector2(1, 0)
 
+var grabbable_block : Block = null
+var carried_block : Block = null
+
 func _physics_process(delta):
-	var wished_dir = 0
-	if Input.is_action_pressed("move_left"):
-		wished_dir = -1
-	elif Input.is_action_pressed("move_right"):
-		wished_dir = 1
-	elif velocity.x != 0:
+	var wished_dir = compute_wished_dir()
+	
+	if wished_dir == 0:
 		var current_velocity = velocity.x
 		velocity.x -= deceleration * sign(velocity.x)
 		if sign(velocity.x) != sign(current_velocity):
@@ -50,37 +48,33 @@ func _physics_process(delta):
 	var acceleration = calc_acceleration(wished_dir)
 	velocity.x = clamp(velocity.x + speed * acceleration * delta, -MAX_VELOCITY, MAX_VELOCITY)
 	
-	print(gravity)
 	velocity += gravity
-	time_since_last_jump += delta
 	
-	if jumped_last_frame:
-		jumped_last_frame = false
-	elif velocity.y != 0 && sign(velocity.y) != sign(prev_velocity.y):
-		gravity.y = STRONG_GRAVITY
+	process_jump(delta)
 	
 	velocity = move_and_slide(velocity, Vector2(0, -1))
+	
 	if is_on_floor():
 		velocity.y = 0
 		jump_count = 0
 	
-	# Flip player
-	if velocity.x != 0:
-		if sign(velocity.x) != sign(prev_velocity.x):
-			scale.x *= -1
-		prev_velocity = velocity
+	flip_player_when_direction_changes()
+	
+	if carried_block:
+		carried_block.global_position = $BlockCarryingPos.global_position
 		
 
 func _input(event):
 	if event.is_action_pressed("throw_block"):
 		if grabbable_block:
-			grabbable_block.throw(throw_impulse)
+			carried_block = grabbable_block
+			grabbable_block = null
 	elif event.is_action_pressed("jump"):
 		jump()
 
 
 func _on_GrabBlockArea_body_entered(body):
-	if body.is_in_group("blocks"):
+	if body.is_in_group("blocks") and !carried_block:
 		grabbable_block = body as Block
 
 
@@ -101,3 +95,28 @@ func jump():
 func calc_acceleration(wished_dir : int):
 	var delta_v = abs(MAX_VELOCITY * wished_dir - velocity.x) / MAX_VELOCITY
 	return wished_dir * lerp(MIN_ACCELERATION, MAX_ACCELERATION, delta_v) 
+
+
+func flip_player_when_direction_changes():
+	if velocity.x != 0:
+		if sign(velocity.x) != sign(prev_velocity.x):
+			scale.x *= -1
+		prev_velocity = velocity
+		
+		
+func process_jump(delta):
+	time_since_last_jump += delta
+	
+	if jumped_last_frame:
+		jumped_last_frame = false
+	elif velocity.y != 0 && sign(velocity.y) != sign(prev_velocity.y):
+		gravity.y = STRONG_GRAVITY
+		
+		
+func compute_wished_dir() -> int:
+	var wished_dir = 0
+	if Input.is_action_pressed("move_left"):
+		wished_dir = -1
+	elif Input.is_action_pressed("move_right"):
+		wished_dir = 1
+	return wished_dir
