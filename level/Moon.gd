97cb607d1edec95b5e7eee4562f07e5065558523
silverweed <silverweed1991@gmail.com>
2019-01-27 11:55:12 +0100

    menuuuuuuuuuhhhh

--- level/Moon.gd
+++ level/Moon.gd
@@ -4,6 +4,9 @@ extends Sprite
 onready var timer = $Timer
 
 
+func _ready():
+	$HaloAnimPlayer.play("pulse")
+
 func _on_Timer_timeout():
 	$AnimationPlayer.play("idle")
 	timer.wait_time = rand_range(2, 10)
