28b8e9626e8528a426061006d9ef536753e3409e
silverweed <silverweed1991@gmail.com>
2019-01-27 09:18:04 +0100

    add menu, ignore imports

---
+++ menu/Menu.gd
@@ -0,0 +1,17 @@
+extends Control
+
+const MAIN_SCENE = preload("res://tests/TestMap.tscn")
+
+
+func set_expanded(node_path : String, expand : bool):
+	var a = 1.3 if expand else 1
+	var ctrl = get_node(node_path)
+	ctrl.rect_scale = Vector2(a, a)
+
+
+func _on_Play_pressed():
+	get_tree().change_scene_to(MAIN_SCENE)
+
+
+func _on_Quit_pressed():
+	get_tree().quit()
